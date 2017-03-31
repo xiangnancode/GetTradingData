5d51da2b5e070c1a4b58148e0008a65090a2d355
xiangnancode <xiangnan.g12@gmail.com>
2017-03-30 23:45:59 -0400

    output to file feature finished

--- readdata.cpp
+++ readdata.cpp
@@ -5,16 +5,17 @@
 //#include <vector>
 #include <iostream>
 #include <fstream>
-//#include <sstream>
+#include <sstream>
 #include <string>
 //#include <stdio.h>
 #include "curl/curl.h"
+#include <ctime>
 //#include "/usr/include/curl/easy.h"
 using namespace std;
 
 string loadticker() {
   ifstream file ("stocksymbols");
-  cout << "reading... " << endl;
+  //cout << "reading tickers... " << endl;
   string entry;
   //while (file.good()) 
   {
@@ -26,13 +27,13 @@ string loadticker() {
 
 void outputdata(string entry) {
   ofstream file;
-  file.open("./output/output1");
+  file.open("./output/output1", std::ios_base::app);
   file << entry;
   file.close();
 }
 
 int writer(char *data, size_t size, size_t nmemb, string *buffer){
-	fprintf(stderr,"Hello I am a function pointer\n");
+	//fprintf(stderr,"Hello I am a function pointer\n");
 	int result = 0;
 	if(buffer != NULL) {
 		buffer -> append(data, size * nmemb);
@@ -41,47 +42,96 @@ int writer(char *data, size_t size, size_t nmemb, string *buffer){
 	return result;
 } 
 
+string getonlinedata(){
+  /* (A) Variable Declaration */
+  CURL *curl;   /* That is the connection, see: curl_easy_init */
+  //CURLcode res;   /* Not needed, see: curl_easy_cleanup */
+  string buffer;    /* See: CURLOPT_WRITEDATA */
+  string tickers;
+  tickers = loadticker();
+  //cout << tickers << endl;
+  string url = "http://finance.google.com/finance/info?q="+tickers;
+  //cout << url << endl;
+  
+  /* (B) Initilise web query */
+  curl = curl_easy_init();
+  
+  /* (C) Set Options of the web query 
+   * See also:  http://curl.haxx.se/libcurl/c/curl_easy_setopt.html  */
+  if (curl){
+    //curl_easy_setopt(curl, CURLOPT_URL, "http://ichart.finance.yahoo.com/table.csv?s=DAI.DE&a=NaN&b=02&c=pr-2&g=d&ignore=.csv");
+    curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
+    curl_easy_setopt(curl, CURLOPT_HEADER, 0);   /* No we don't need the Header of the web content. Set to 0 and curl ignores the first line */
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 0); /* Don't follow anything else than the particular url requested*/
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);  /* Function Pointer "writer" manages the required buffer size */
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer ); /* Data Pointer &buffer stores downloaded web content */ 
+  }
+  else{
+    fprintf(stderr,"Error 1\n");  /* Badly written error message */
+    return 0;                     
+  }
+  
+  /* (D) Fetch the data */
+    curl_easy_perform(curl);
+  /* res = curl_easy_perform(curl); */
+  /* There is no need for "res" as it is just a flag */ 
+  
+  /* (E) Close the connection */
+  curl_easy_cleanup(curl);
+  return buffer;
+}
+
+string pickdata(string rawdata) {
+  string res;
+  size_t position;
+  position = rawdata.find("ltt");
+  res = rawdata.substr(position + 6,7);
+
+  istringstream rawdatastream(rawdata);
+  string ticker, entry;
+  while (getline (rawdatastream, ticker, '}'))
+  {
+    //getline (rawdatastream, ticker, '}');
+    istringstream tickerstream(ticker);
+    getline(tickerstream, entry, 'l');
+    getline(tickerstream, entry, '"');
+    getline(tickerstream, entry, '"');
+    if(getline(tickerstream, entry, '"')) {
+      res += ',';
+      res.append(entry);
+    }
+  }
+  res += '\n';
+  return res;
+}
+
 
 
 int main ()
 {
-	/* (A) Variable Declaration */
-	CURL *curl;		/* That is the connection, see: curl_easy_init */
-	//CURLcode res;		/* Not needed, see: curl_easy_cleanup */
-	string buffer;		/* See: CURLOPT_WRITEDATA */
-  string tickers;
-  tickers = loadticker();
-  cout << tickers << endl;
-  string url = "http://finance.google.com/finance/info?q="+tickers;
-  cout << url << endl;
-	
-	/* (B) Initilise web query */
-	curl = curl_easy_init();
-	
-	/* (C) Set Options of the web query 
-	 * See also:  http://curl.haxx.se/libcurl/c/curl_easy_setopt.html  */
-	if (curl){
-		//curl_easy_setopt(curl, CURLOPT_URL, "http://ichart.finance.yahoo.com/table.csv?s=DAI.DE&a=NaN&b=02&c=pr-2&g=d&ignore=.csv");
-		curl_easy_setopt(curl, CURLOPT_URL, &url[0]);
-		curl_easy_setopt(curl, CURLOPT_HEADER, 0);	 /* No we don't need the Header of the web content. Set to 0 and curl ignores the first line */
-		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 0); /* Don't follow anything else than the particular url requested*/
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);	/* Function Pointer "writer" manages the required buffer size */
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer ); /* Data Pointer &buffer stores downloaded web content */	
-	}
-	else{
-		fprintf(stderr,"Error 1\n");	/* Badly written error message */
-		return 0;											
-	}
-	
-	/* (D) Fetch the data */
-    curl_easy_perform(curl);
-	/* res = curl_easy_perform(curl); */
-	/* There is no need for "res" as it is just a flag */	
-	
-	/* (E) Close the connection */
-	curl_easy_cleanup(curl);
-	cout << buffer << endl;
-  outputdata(buffer);
+  string rawdata;
+  string picked;
+  rawdata = getonlinedata();
+	//cout << rawdata << endl;
+  
+  //cout << "time, " << loadticker() << endl;
+  time_t now = time(0);
+  int before = now - 60, count = 0;
+  while(1) {
+    now = time(0);
+    if (now - before >= 60) {
+      //cout << now << endl;
+      before = now;
+      picked = pickdata(rawdata);
+      cout << picked;
+      outputdata(picked);
+      ++count;
+      if (count == 5) break;
+    }
+  }
+
+
+  //cout << before << endl;
 
 
 	/*
